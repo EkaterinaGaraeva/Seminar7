2dcddd050fe040f3b32c14e2653e4cc3701fa732
Ваше имя <garaevaea@mail.ru>
2022-04-07 08:14:09 +0300

    Add Task49

---
+++ Task49/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Задача 49: Задайте двумерный массив. 
+Найдите элементы, у которых оба индекса чётные, 
+и замените эти элементы на их квадраты.
+Например, изначально массив
+выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Новый массив будет выглядеть
+вот так:
+1 4 7 2
+5 81 2 9
+8 4 2 4
+*/
+
+int[,] Generate2DArray(int higth, int weigth, int delta)
+{
+    int[,] array2D = new int[higth, weigth];
+    for (int i = 0; i < higth; i++)
+    {
+        for (int j = 0; j < weigth; j++)
+        {
+            array2D[i, j] = new Random().Next(delta * -1, delta);
+        }
+    }
+    return array2D;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write($"{arrayToPrint}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FindEvenElements(int[,] arrayToFind)
+{
+    for (int i = 0; i < arrayToFind.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayToFind.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                arrayToFind[i,j] = arrayToFind[i,j] * arrayToFind[i,j];
+            }
+        }
+    }
+    return arrayToFind;
+}
+
+int[,] newArray = Generate2DArray(3, 4, 9);
+Print2DArray(newArray);
+Console.WriteLine();
+int[,] modifiedArray = FindEvenElements(newArray);
+Print2DArray(modifiedArray);
